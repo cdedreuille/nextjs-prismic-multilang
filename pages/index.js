c281b701f8fb74ac3ab0771f0820a5ce0687ae56
Eric Bond <ericbond007@gmail.com>
2018-04-26 18:05:27 -0400

    index shows home page text from prismic

--- pages/index.js
+++ pages/index.js
@@ -1,33 +1,29 @@
+import React from 'react'
 import Link from 'next/link'
 import Head from '../components/head'
 import Nav from '../components/nav'
-
+import { RichText, Date } from 'prismic-reactjs'
 import Prismic from 'prismic-javascript'
 
 
-const Index = (props) => (
-  <div>
-    <Head title="Home" />
-    <Nav />
+const apiEndpoint = 'https://charles-test-nextjs.cdn.prismic.io/api/v2';
 
-    <div>HomePage</div>
-  </div>
-)
+export default class Index extends React.Component {
+  static async getInitialProps({ req, query }) {
+    const apiData = await Prismic.getApi(apiEndpoint)
+      .then(api => {
+        return api.query( Prismic.Predicates.at('document.type', 'homepage'));
+      })
+      .catch(err => console.log(err));
 
-Index.getInitialProps = async function() {
-  const apiEndpoint = 'https://charles-test-nextjs.cdn.prismic.io/api/v2';
+    return { homepage: apiData.results };
+  }
 
-  let res = [];
-  Prismic.api(apiEndpoint).then(api => {
-    api.query(Prismic.Predicates.at('document.type', 'homepage')).then(response => {
-      if (response) {
-        res = response.results[0];
-      }
-    });
-  });
-
-  return res;
-
+  render() {
+    return (
+      <div>
+        <h1>{this.props.homepage[0].data.home_page_header[0].text}</h1>
+      </div>
+    ); 
+  }
 }
-
-export default Index
