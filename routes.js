f8d872693649b5b3e683254f03422163e341b78d
Eric Bond <ericbond007@gmail.com>
2018-04-30 14:51:15 -0400

    /fr-fr and /en-gb now working

--- routes.js
+++ routes.js
@@ -1,6 +1,8 @@
-const routes = module.exports = require('next-routes-with-locale')({ locale: 'en-gb' })
+const routes = module.exports = require('next-routes-with-locale')({ locale: 'fr-fr' })
 
 routes
+  .add('index', 'en-gb', '/')
+  .add('index', 'fr-fr', '/')
   .add('faq', 'en-gb', '/faq')
   .add('faq', 'fr-fr', '/faq')
   .add('blog', 'en-gb', '/blog')
